30f710aba5f8f94849912490a74913b89d35111f
Mayara Santos <mayara.santos@catho.com>
2018-02-08 14:39:44 -0200

    Render index.html

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+var path = require('path')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
