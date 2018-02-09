394abe38aae98f752d273f49029735ed3c9e9278
Mayara Santos <mayara.santos@catho.com>
2018-02-09 10:14:52 -0200

    public directory

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.use('/static',express.static('public'));
+// app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
