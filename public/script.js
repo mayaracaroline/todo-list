8a8ec3ea2955e1df3c4cf1116c3498df461c45b6
Mayara Santos <mayara.santos@catho.com>
2018-03-15 10:34:08 -0300

    fix identation

--- public/script.js
+++ public/script.js
@@ -1,35 +1,37 @@
-(function(){var btnAddItem = document.getElementById("button-add-item");
-var numberItem = 1;
+(function() {
+  var btnAddItem = document.getElementById("button-add-item");
+  var numberItem = 1;
 
-//functions
-function addItem() {
-	//Declarações:
-	var label = document.createElement('label');
-	var checkbox = document.createElement('input');
-	var ul = document.getElementById('container');
-	var li = document.createElement('li');
-	
-	//Atribuições:
-	checkbox.setAttribute('id','checkbox-add-item-' + (numberItem));
-	checkbox.setAttribute('type','checkbox');
-	label.appendChild(checkbox);
-	label.appendChild(document.createTextNode(document.getElementById("input-add-item").value));
+  //functions
+  function addItem() {
+  	//Declarações:
+  	var label = document.createElement('label');
+  	var checkbox = document.createElement('input');
+  	var ul = document.getElementById('container');
+  	var li = document.createElement('li');
+  	
+  	//Atribuições:
+  	checkbox.setAttribute('id','checkbox-add-item-' + (numberItem));
+  	checkbox.setAttribute('type','checkbox');
+  	label.appendChild(checkbox);
+  	label.appendChild(document.createTextNode(document.getElementById("input-add-item").value));
 
-	//Append:
-	//li.appendChild(checkbox);
-	li.appendChild(label);
-	ul.appendChild(li);
+  	//Append:
+  	//li.appendChild(checkbox);
+  	li.appendChild(label);
+  	ul.appendChild(li);
 
-	document.getElementById("input-add-item").value='';
-	numberItem++;
-}
+  	document.getElementById("input-add-item").value='';
+  	numberItem++;
+  }
 
-// on DOM ready...
-var form = document.getElementById('form-add-item');
-function onSubmit(event) {
+  // on DOM ready...
+  var form = document.getElementById('form-add-item');
+  function onSubmit(event) {
     event.preventDefault();
     addItem();
-}
+  }
 
-form.addEventListener('submit', onSubmit)})();
+  form.addEventListener('submit', onSubmit);
+})();
 
