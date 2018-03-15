93af80c3a1804f6eb9af69228bd741d167c1d917
Mayara Santos <mayara.santos@catho.com>
2018-03-15 09:08:28 -0300

    review

--- public/script.js
+++ public/script.js
@@ -1,10 +1,9 @@
-var btnAddItem = document.getElementById("button-add-item");
+(function(){var btnAddItem = document.getElementById("button-add-item");
 var numberItem = 1;
 
 //functions
 function addItem() {
 	//Declarações:
-	var valueInput = document.getElementById("input-add-item").value;
 	var label = document.createElement('label');
 	var checkbox = document.createElement('input');
 	var ul = document.getElementById('container');
@@ -13,11 +12,11 @@ function addItem() {
 	//Atribuições:
 	checkbox.setAttribute('id','checkbox-add-item-' + (numberItem));
 	checkbox.setAttribute('type','checkbox');
-	label.setAttribute('for','checkbox-add-item-' + (numberItem));
-	label.innerHTML = valueInput;
+	label.appendChild(checkbox);
+	label.appendChild(document.createTextNode(document.getElementById("input-add-item").value));
 
 	//Append:
-	li.appendChild(checkbox);
+	//li.appendChild(checkbox);
 	li.appendChild(label);
 	ul.appendChild(li);
 
@@ -28,22 +27,9 @@ function addItem() {
 // on DOM ready...
 var form = document.getElementById('form-add-item');
 function onSubmit(event) {
-    if (event) { event.preventDefault(); }
-    // do AJAX stuff 
-    console.log('submitted');
+    event.preventDefault();
+    addItem();
 }
 
-form.addEventListener('submit', onSubmit, false);
-form.submit = onSubmit;
-console.log(urlEncodeFormData(form));
+form.addEventListener('submit', onSubmit)})();
 
-function urlEncodeFormData(form) {
-    var i, e, data = [];
-    for (i = 0; i < form.elements.length; i++) {
-        e = form.elements[i];
-        if (e.type !== 'button' && e.type !== 'submit') {
-        	data.push(encodeURIComponent(e.id) + '=' + encodeURIComponent(e.value)); 
-        }
-    };
-    return data.join('&');
-}
