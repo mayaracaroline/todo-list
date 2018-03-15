d8c444c9b707782849e365b3632ca8cebf5c7010
Mayara Santos <mayara.santos@catho.com>
2018-03-15 10:39:44 -0300

    Corrigindo identação

--- public/script.js
+++ public/script.js
@@ -4,26 +4,25 @@
 
   //functions
   function addItem() {
-  	//Declarações:
-  	var label = document.createElement('label');
-  	var checkbox = document.createElement('input');
-  	var ul = document.getElementById('container');
-  	var li = document.createElement('li');
-  	
-  	//Atribuições:
-  	checkbox.setAttribute('id','checkbox-add-item-' + (numberItem));
-  	checkbox.setAttribute('type','checkbox');
-  	label.appendChild(checkbox);
-  	label.appendChild(document.createTextNode(document.getElementById("input-add-item").value));
+	//Declarações:
+	var label = document.createElement('label');
+	var checkbox = document.createElement('input');
+	var ul = document.getElementById('container');
+	var li = document.createElement('li');
+	
+	//Atribuições:
+	checkbox.setAttribute('id','checkbox-add-item-' + (numberItem));
+	checkbox.setAttribute('type','checkbox');
+	label.appendChild(checkbox);
+	label.appendChild(document.createTextNode(document.getElementById("input-add-item").value));
 
-  	//Append:
-  	//li.appendChild(checkbox);
-  	li.appendChild(label);
-  	ul.appendChild(li);
+	//Append:
+	li.appendChild(label);
+	ul.appendChild(li);
 
-  	document.getElementById("input-add-item").value='';
-  	numberItem++;
-  }
+	document.getElementById("input-add-item").value='';
+	numberItem++;
+}
 
   // on DOM ready...
   var form = document.getElementById('form-add-item');
